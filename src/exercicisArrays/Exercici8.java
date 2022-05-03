016b4cabc1086227e4a2b086dcc931084e2e5d68
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-05-03 16:14:38 +0200

    Exercici8

--- src/exercicisArrays/Exercici8.java
+++ src/exercicisArrays/Exercici8.java
@@ -12,9 +12,23 @@ public class Exercici8 {
 		System.out.println("]");
 	}
 	
+	public double calcularMitjana(double[] cadena) {
+		double suma = 0;
+		double mitjana;
+		for (int i = 0; i < cadena.length; i++) {
+			suma+=cadena[i];
+		}
+		mitjana = suma / cadena.length;
+		return mitjana;
+	}
+	
 	
 	public static void main(String[] args) {
-
+		Exercici8 p = new Exercici8();
+		double[] cadenaDouble = {7, 8, 5, 7, 3, 2};
+		p.mostraNotes(cadenaDouble);
+		
+		System.out.println("La mitjana de la classe és: " + p.calcularMitjana(cadenaDouble));
 	}
 
 }
